7cc6390294b1371577b664949f89dc67165c8c1f
David Vielhuber <david@vielhuber.de>
2016-10-31 14:33:02 +0100

    initial push

---
+++ script.js
@@ -0,0 +1,77 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    if( document.querySelectorAll('.socialfoo').length > 0 ) {
+    	document.querySelectorAll('.socialfoo').forEach(function(el) {
+    		
+    		var socialfoo_div = document.createElement("div");
+    		socialfoo_div.className = 'socialfoo-container';
+    		var socialfoo_ul = document.createElement("ul");
+    		socialfoo_div.appendChild(socialfoo_ul);
+    		el.parentNode.insertBefore(socialfoo_div, el);
+    		el.parentNode.removeChild(el);
+
+			var item_url = el.getAttribute('data-url');
+			var item_title = el.getAttribute('data-title');
+			var item_image = el.getAttribute('data-image');
+
+			// total
+			var socialfoo_item_total_li = document.createElement('li');
+			var socialfoo_item_total_label = document.createElement('label');
+			var socialfoo_item_total_span = document.createElement('span');
+			socialfoo_item_total_label.innerHTML = 'Total:';
+			socialfoo_item_total_span.setAttribute('class','count');
+			socialfoo_item_total_li.setAttribute('class','total');
+			socialfoo_item_total_li.appendChild(socialfoo_item_total_label);
+			socialfoo_item_total_li.appendChild(socialfoo_item_total_span);
+			socialfoo_ul.appendChild(socialfoo_item_total_li);
+			
+			["google", "facebook", "xing", "linkedin", "twitter", "pinterest", "mail"].forEach(function(item) {
+				var href = "#";
+				if( item == 'google' ) { href = 'https://plus.google.com/share?url='+encodeURIComponent(item_url)+''; }
+				if( item == 'facebook' ) { href = 'https://www.facebook.com/sharer/sharer.php?u='+encodeURIComponent(item_url)+''; }
+				if( item == 'xing' ) { href = 'https://www.xing.com/app/user?op=share&amp;url='+encodeURIComponent(item_url)+';title='+encodeURIComponent(item_title)+''; }
+				if( item == 'linkedin' ) { href = 'https://www.linkedin.com/shareArticle?mini=true&url='+encodeURIComponent(item_url)+'&title='+encodeURIComponent(item_title)+'&summary=&source='; }
+				if( item == 'twitter' ) { href = 'https://twitter.com/home?status='+encodeURIComponent(item_title+' - '+item_url)+''; }
+				if( item == 'mail' ) { href = 'mailto: ?subject='+item_title+'&body='+item_url+''; }
+				if( item == 'pinterest' ) { if( item_image === null ) { return; } href = 'http://pinterest.com/pin/create/button/?url='+encodeURIComponent(item_url)+'&media='+encodeURIComponent(item_image)+'&description='+encodeURIComponent(item_title)+''; }
+				var socialfoo_item_li = document.createElement('li');
+				var socialfoo_item_a = document.createElement('a');
+				var socialfoo_item_label = document.createElement('label');
+				var socialfoo_item_span = document.createElement('span');
+				socialfoo_item_li.setAttribute('class',item);
+				socialfoo_item_a.setAttribute('target','_blank');
+				socialfoo_item_a.setAttribute('rel','nofollow');
+				socialfoo_item_a.setAttribute('href',href);
+				socialfoo_item_label.innerHTML = item;
+				socialfoo_item_span.setAttribute('class','count');
+				socialfoo_item_a.appendChild(socialfoo_item_label);
+				socialfoo_item_a.appendChild(socialfoo_item_span);
+				socialfoo_item_li.appendChild(socialfoo_item_a);
+				socialfoo_ul.appendChild(socialfoo_item_li);
+			});
+
+			var xhr = new XMLHttpRequest()
+			xhr.open('POST', "script.php", true);
+			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+			xhr.onreadystatechange = function () {
+				if (this.readyState != 4 || this.status != 200) return;
+				var data = JSON.parse(this.responseText);
+
+				// set total
+				document.querySelector(".total .count").innerHTML = data['total'];
+
+				// set individual
+				["google", "facebook", "xing", "linkedin", "pinterest"].forEach(function(item) {
+					var socialfoo_item_span = document.querySelector("." + item + " .count");
+					socialfoo_item_span.innerHTML = data[item];
+				});
+			}
+			xhr.send("url="+item_url);
+
+    	});
+    }
+
+});
---
+++ script.php
@@ -0,0 +1,98 @@
+<?php
+class SocialFoo
+{
+
+   public $url = null;
+   
+   public function __construct($url)
+   {
+      $this->url = $url;
+   }
+   
+   public function getCounts()
+   {      
+      $result = [];
+      $result["url"] = $this->url;
+      $result["facebook"] = $this->getCountFacebook();
+      $result["pinterest"] = $this->getCountPinterest();
+      $result["linkedin"] = $this->getCountLinkedIn();
+      $result["google"] = $this->getCountGoogle();
+      $result["xing"] = $this->getCountXing();
+      $result["total"] = $result["facebook"]["shares"] + $result["pinterest"] + $result["linkedin"] + $result["google"] + $result["xing"];
+      return $result;
+   }
+
+   public function getCountFacebook()
+   {
+        $api = file_get_contents( 'https://graph.facebook.com/?id=' . $this->url );
+        $count = json_decode( $api );
+        // if you want to get commments, use instead $count->share->comment_count
+        if(isset($count->share->share_count) && $count->share->share_count != '0'){
+            return intval($count->share->share_count);
+        }
+        return 0;
+   }
+
+   public function getCountPinterest()
+   {
+      $api = file_get_contents( 'https://api.pinterest.com/v1/urls/count.json?callback%20&url=' . $this->url );
+      $count = preg_replace( '/^receiveCount\((.*)\)$/', '\\1', $api );
+      $count = json_decode( $count );
+      if(isset($count->count) && $count->count != '0') {
+         return intval($count->count);
+      }
+      return 0;
+   }
+
+   public function getCountLinkedIn()
+   {
+      $api = file_get_contents( 'https://www.linkedin.com/countserv/count/share?url=' . $this->url . '&format=json' );
+      $count = json_decode( $api );
+      if(isset($count->count) && $count->count != '0') {
+         return intval($count->count);
+      }
+      return 0;
+   }
+
+   public function getCountGoogle()
+   {
+      /* this solution does not use curl or an api key */
+      $content = file_get_contents("https://plusone.google.com/u/0/_/+1/fastbutton?url=".urlencode($this->url)."&count=true");
+      $doc = new DOMdocument();
+      libxml_use_internal_errors(true);
+      $doc->loadHTML($content);
+      $doc->saveHTML();
+      $num = $doc->getElementById('aggregateCount')->textContent;
+      if($num != "") {
+         $num = str_replace(".","",$num);
+         $num = str_replace(">","",$num);
+         return intval($num);
+      }
+      return 0;
+   }
+
+   public function getCountXing()
+   {
+      /* this solution does not use curl or an api key */
+      $content = file_get_contents("https://www.xing-share.com/app/share?op=get_share_button;counter=top;url=".$this->url);
+      $doc = new DOMdocument();      
+      libxml_use_internal_errors(true);
+      $doc->saveHTML();
+      $doc->loadHTML($content);
+      $finder = new DomXPath($doc);
+      $classname = "xing-count";
+      $spans = $finder->query("//*[contains(@class, '$classname')]");
+      if( $spans->length > 0 ) {
+         return $spans->item(0)->firstChild->nodeValue;
+      }
+      return 0;
+   }
+
+}
+
+if( !isset($_POST["url"]) || $_POST["url"] == "" ) { die(); }
+$url = strip_tags($_POST["url"]);
+$socialfoo = new SocialFoo($url);
+$result = $socialfoo->getCounts();
+echo json_encode($result);
+die();
